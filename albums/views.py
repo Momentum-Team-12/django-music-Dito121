e5fbdd79a521e334b65b73f0e05db213badc56de
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-04 17:58:35 -0400

    form needs to be fixed, add, edit, and delete

--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from albums.models import Album
+from albums.forms import AlbumForm
 
 
 def list_albums(request):
@@ -11,3 +12,38 @@ def album_detail(request, pk):
     context = {"album": album}
     return render(request, "albums/album_detail.html", context)
 
+def add_album(request):
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+
+    return render(request, "albums/add_album.html", {"form": form})
+
+
+def edit_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'GET':
+        form = AlbumForm(instance=album)
+    else:
+        form = AlbumForm(data=request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+
+    return render(request, "albums/edit_album.html", {
+        "form": form,
+        "album": album
+    })
+
+
+def delete_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'POST':
+        album.delete()
+        return redirect(to='list_albums')
+
+    return render(request, "albums/delete_album.html", {"album": album})
