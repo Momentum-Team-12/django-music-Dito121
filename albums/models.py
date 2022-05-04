56181b69cf2d41ed7b0d2b57d8d7ca8e852b892b
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-04 16:03:04 -0400

    detail page done

--- albums/models.py
+++ albums/models.py
@@ -5,5 +5,7 @@ class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     image = models.FilePathField(path="/img")
-    created_at = models.DateTimeField(auto_now_add=True)
-    released = models.DateField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    released = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
--- albums/views.py
+++ albums/views.py
@@ -5,3 +5,9 @@ from albums.models import Album
 def list_albums(request):
     albums = Album.objects.all()
     return render(request, "albums/list_albums.html", {'albums': albums})
+
+def album_detail(request, pk):
+    album = Album.objects.get(pk=pk)
+    context = {"album": album}
+    return render(request, "albums/album_detail.html", context)
+
