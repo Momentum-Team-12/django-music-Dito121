bf7be670838a199ca3cb1ef42870a68e3c189399
Gerardo Ayes <gerardoayes92@gmail.com>
2022-05-05 12:41:11 -0400

    trying to get pictures to work

--- albums/models.py
+++ albums/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.conf import settings
 
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
-    image = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
+    image = models.ImageField(upload_to='img/')
     created = models.DateTimeField(auto_now_add=True)
     released = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
